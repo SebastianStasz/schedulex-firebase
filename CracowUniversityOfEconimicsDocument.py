099536989e8f2de887d7eb4d78301fa02497d819
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-07-23 17:41:24 +0200

    Code refactor + checking size of the document

---
+++ CracowUniversityOfEconimicsDocument.py
@@ -0,0 +1,22 @@
+from firestore_size.calculate import document_size
+
+
+class CracowUniversityOfEconimicsDocument:
+    def __init__(self, db):
+        self.db = db
+
+
+    def set_document_data(self, uek_data):
+        doc_ref = self.document_reference(self.db)
+        doc_ref.set(uek_data)
+
+
+    def check_document_size(self):
+        doc_ref = self.document_reference()
+        data = doc_ref.get().to_dict()
+        doc_size = document_size(data)
+        print(f'cracow_university_of_economics document size is {doc_size} bytes.')
+
+
+    def document_reference(self):
+        return self.db.collection('schools').document('cracow_university_of_economics')
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from uek_schedule_scraper import get_cracow_univeristy_of_economics_data
-from schedulex_firebase import save_uek_data
+from firebase_admin import credentials, firestore
+import CracowUniversityOfEconimicsDocument
+import firebase_admin
+
+
+def initialize_app():
+    cred = credentials.Certificate("schedulex-service-account-key.json")
+    firebase_admin.initialize_app(cred)
 
 
 if __name__ == '__main__':
+    initialize_app()
+    db = firestore.client()
+    uekDocument = CracowUniversityOfEconimicsDocument(db)
     data = get_cracow_univeristy_of_economics_data()
-    save_uek_data(data)
+    uekDocument.set_document_data(data)
+    uekDocument.check_document_size()
