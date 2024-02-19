f3b56ed0c50a407b633b94b4d8d52fa257c67a2e
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2024-02-19 19:27:02 +0100

    Retry in case of error

--- uek_schedule_scraper_utils.py
+++ uek_schedule_scraper_utils.py
@@ -1,5 +1,7 @@
 import bs4
 import requests
+from requests.adapters import HTTPAdapter, Retry
+
 
 
 base_url = 'https://planzajec.uek.krakow.pl/index.php/'
@@ -11,8 +13,26 @@ def get_url_for_faculty_group_events(url_suffix):
 
 
 def get_soup_from_url(url):
-    response = requests.get(url)
-    return bs4.BeautifulSoup(response.content, 'html.parser')
+    max_retry = 5
+    number_of_requests = 0
+    response_result = None
+    
+    retry_strategy = Retry(total=100)
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    session = requests.Session()
+    session.mount('https://', adapter)
+
+    while number_of_requests < max_retry:
+        try:
+            response = session.get(url)
+            if response.status_code == requests.codes.ok:
+                response_result = response
+                break
+        except requests.exceptions.RequestException as e:
+            print(f'Error: {e}')
+            number_of_requests += 1
+
+    return bs4.BeautifulSoup(response_result.content, 'html.parser')
 
 
 def to_faculty_group_name_document(group_name):
